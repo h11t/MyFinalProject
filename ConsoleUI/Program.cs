47ad07930b41ff57c55e04810caef5ed2a1c1c2f
Hilal Tekdemir <hilal.tek.demir00@gmail.com>
2021-03-03 23:04:14 +0300

    10.ders

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -24,10 +24,20 @@ namespace ConsoleUI
         private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EFProductDal());
-            foreach (var item in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+            if (result.Success)
             {
-                Console.WriteLine(item.ProductName + " - " + item.CategoryName);
+                Console.WriteLine(result.Message);
+                foreach (var item in result.Data)
+                {
+                    Console.WriteLine(item.ProductName + " - " + item.CategoryName);
+                }
             }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+            
         }
     }
 }
